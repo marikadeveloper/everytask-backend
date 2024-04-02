b92717ff5cb5e5ee4d2858c9c4c1949098671b57
Marika Mella <marika.developer@gmail.com>
2024-04-02 21:10:41 +0200

    Fixed unique constraint

--- src/utils/callbacks.ts
+++ src/utils/callbacks.ts
@@ -64,7 +64,7 @@ export async function updateTaskDailyStat({
 
   // fetch the TaskDailyStat for the current user and date
   const foundTaskDailyStat = await prisma.taskDailyStat.findUnique({
-    where: { userId_date: { userId, date: now } },
+    where: { userId: userId, date: now },
   });
 
   if (foundTaskDailyStat) {
